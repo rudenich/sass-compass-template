ce6529e892ba4e7c7022197b3c36ac678d24da0e
Rudenich <rudenich@gmail.com>
2015-03-11 10:44:30 +0200

    some fixes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var markup = new Markup({
+        overlay: {
+            images: {
+                '_default_':
+                    ['index.jpg']
+            }
+        }
+    });
+});
